59c5d17d688258627ac3f6814c25effaee2e36f0
Paulo Victor Campelo Selano <pv_sf@hotmail.com>
2018-08-29 23:00:01 -0300

    currently reading header

---
+++ client/src/BookCard/BookCard.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+class BookCard extends React.Component {
+  render() {
+    return (
+      <div className="book-card">
+      </div>
+    );
+  }
+}
+
+export default BookCard;
--- client/src/CurrentlyReading/CurrentlyReading.js
+++ client/src/CurrentlyReading/CurrentlyReading.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import BookCard from '../BookCard/BookCard';
 
 class CurrentlyReading extends React.Component {
   render() {
     return (
       <div className="currently-reading">
         <div className="currently-reading__header">Currently Reading</div>
+        <BookCard />
       </div>
     );
   }
