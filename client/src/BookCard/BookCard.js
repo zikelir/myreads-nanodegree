266c52f54509d82b38d0fc5506712bf8470cd43f
Paulo Victor Campelo Selano <pv_sf@hotmail.com>
2018-08-30 22:29:26 -0300

    cards initial

--- client/src/BookCard/BookCard.js
+++ client/src/BookCard/BookCard.js
@@ -1,12 +1,34 @@
-import React from 'react';
+import React from "react";
 
 class BookCard extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
   render() {
+    const { allBooks } = this.props;
     return (
-      <div className="book-card">
+      <div className="book-cards">
+      {console.log(allBooks)}
+        {allBooks &&
+            allBooks.map(item => (
+              <div className="book-card">
+                <div className="book-card__header" style={{backgroundImage: `URL(${item.imageLinks.thumbnail})`}}>
+                  <div className="book-card__info">{item.title}</div>
+                </div>
+                <div className="book-card__body">{' '}</div>
+              </div>
+            ))
+        }
       </div>
     );
   }
 }
 
 export default BookCard;
+{
+  /* <div className="book-card">
+        <div className="book-card__header"></div>
+        <div className="book-card__body"></div>
+      </div> */
+}
--- client/src/CurrentlyReading/CurrentlyReading.js
+++ client/src/CurrentlyReading/CurrentlyReading.js
@@ -1,12 +1,20 @@
 import React from 'react';
 import BookCard from '../BookCard/BookCard';
-
+import { getAll } from "../../BooksAPI.js";
 class CurrentlyReading extends React.Component {
+  state = { allBooks: [] };
+
+  componentDidMount() {
+    getAll().then(response => {
+      this.setState({ allBooks: response });
+    });
+  }
+
   render() {
     return (
       <div className="currently-reading">
         <div className="currently-reading__header">Currently Reading</div>
-        <BookCard />
+        <BookCard allBooks={this.state.allBooks}/>
       </div>
     );
   }
