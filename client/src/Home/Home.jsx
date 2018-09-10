9e9e23b3f9841fb6dc00d9fcc4d3770d8fcd5333
Paulo Victor Campelo Selano <pv_sf@hotmail.com>
2018-09-09 21:42:44 -0300

    added proptypes to home.js

--- client/src/Home/Home.jsx
+++ client/src/Home/Home.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import Shelf from '../Shelf/Shelf.jsx';
 
 const Home = (props) => {
   const {
-    updateBook, currentlyReading, wantToRead, read, none,
+    updateBook, currentlyReading, wantToRead, read,
   } = props;
   return (
     <div className="home">
@@ -12,6 +13,25 @@ const Home = (props) => {
       <Shelf shelf={read} updateBook={updateBook} />
     </div>
   );
+};
+
+Home.propTypes = {
+  updateBook: PropTypes.func.isRequired,
+  currentlyReading: PropTypes.shape({
+    books: PropTypes.array,
+    component: PropTypes.object,
+    css: PropTypes.object,
+  }).isRequired,
+  wantToRead: PropTypes.shape({
+    books: PropTypes.array,
+    component: PropTypes.object,
+    css: PropTypes.object,
+  }).isRequired,
+  read: PropTypes.shape({
+    books: PropTypes.array,
+    component: PropTypes.object,
+    css: PropTypes.object,
+  }).isRequired,
 };
 
 export default Home;
