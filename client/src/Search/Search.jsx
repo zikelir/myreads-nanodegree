ec266382d3759874db10bfa298f2e872cc0970c2
Paulo Victor Campelo Selano <pv_sf@hotmail.com>
2018-09-12 22:15:17 -0300

    small handler for text on search

--- client/src/Search/Search.jsx
+++ client/src/Search/Search.jsx
@@ -1,11 +1,32 @@
-import React from 'react';
+import React from "react";
+import { getAll, search } from "../../BooksAPI";
 
-const Search = (props) => {
-  return (
-    <div className="search">
-      <div>Search books</div>
-    </div>
-  );
-};
+class Search extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      query: '',
+    };
+  }
+
+  componentDidMount() {}
+
+  handleQuery = (e) => {
+    e.preventDefault();
+    this.setState({ query: e.target.value });
+  }
+
+  render() {
+    const { query } = this.state;
+    return (
+      <div className="search">
+        <div />
+        <div>Search books</div>
+        <input type="text" value={query} onChange={this.handleQuery} />
+        {query}
+      </div>
+    );
+  }
+}
 
 export default Search;
